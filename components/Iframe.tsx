c379f907de9197200ceecdb79d4181a65169eb87
Dmitriy Sidorenko <dmitriysidor3nk0@gmail.com>
2022-02-28 17:43:06 +0200

    allow widget in iframe to open popups

--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -3,7 +3,7 @@ import { IWidgetOptions } from "../types";
 function Iframe({ type = "one", position = "bottom-right" }: IWidgetOptions) {
   return (
     <iframe
-      sandbox="allow-scripts allow-same-origin"
+      sandbox="allow-scripts allow-same-origin allow-popups"
       className="w-full h-full border-none"
       src={`/sandbox/${type}/${position}`}
     ></iframe>
