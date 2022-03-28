c1c947d520e288d6888a687bbc219184edfa2acb
Maxim Khmara <maxim.khmara@gearheart.io>
2022-03-28 15:21:24 +0300

    widget list

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -28,17 +28,15 @@ function Widget({ type }: { type: IWidgetType }) {
   }, [])
   return (
     <div className="w-full overflow-hidden md:max-w-6xl">
-      <div className="md:flex">
-        <div className="md:shrink-0">
-          <div
-            className={`h-80 w-full rounded-t-lg md:w-96 bg-gray-100 md:rounded-lg overflow-hidden`}
-          >
+      <div className="flex flex-row flex-wrap">
+        <div className="w-full">
+          <div className={`h-60 w-full rounded-t-lg bg-gray-200 overflow-hidden`}>
             <Iframe type={options.type} position={options.position} />
           </div>
         </div>
 
-        <div className="pl-0 md:pt-0 md:pl-4 w-full h-40 md:h-80">
-          <div className="rounded-b-lg md:rounded-lg border border-gray-200 w-full flex flex-col h-full overflow-hidden">
+        <div className="w-full h-48">
+          <div className="rounded-b-lg border border-gray-200 w-full flex flex-col h-full overflow-hidden">
             <div className={`flex w-full`}>
               <div className={`flex border-r border-r-gray-200 lg:px-4`}>
                 <div className={`flex-none pl-3 py-2 font-medium`}>Placement:</div>
@@ -50,7 +48,7 @@ function Widget({ type }: { type: IWidgetType }) {
                       position: e.target.value as IWidgetOptions['position'],
                     })
                   }
-                  className={`form-select appearance-none font-medium block px-2 py-2 text-base text-blue-600 bg-white bg-clip-padding bg-no-repeat rounded transition ease-in-out m-0 focus:text-blue-700 focus:bg-white focus:border-blue-600 focus:outline-none`}
+                  className={`form-select appearance-none font-medium block px-2 py-2 text-base text-blue-600 hover:text-blue-800 bg-white bg-clip-padding bg-no-repeat rounded transition ease-in-out m-0 focus:text-blue-700 focus:bg-white focus:border-blue-600 focus:outline-none cursor-pointer`}
                 >
                   {Object.entries(widgetPositionsList).map(([position, label]) => {
                     console.log(position, options.position)
@@ -67,7 +65,7 @@ function Widget({ type }: { type: IWidgetType }) {
                 className={`flex-1 text-md font-medium ${
                   isCopied
                     ? `bg-gradient-to-r via-blue-500 from-yellow-300 to-yellow-300 text-white`
-                    : 'text-blue-600'
+                    : 'text-blue-600 hover:text-blue-800'
                 }`}
               >
                 {isCopied ? 'Copied!' : 'Copy to Clipboard'}
--- components/WidgetList.tsx
+++ components/WidgetList.tsx
@@ -1,26 +1,42 @@
 import { widgetTypes } from '../types'
 import Widget from './Widget'
+import { ReactNode } from 'react'
+
+const ListItem = ({ order, children }: { order: number; children: ReactNode }) => {
+  return (
+    <div className="mb-3 flex flex-nowrap items-center">
+      <span className="flex-shrink-0 mr-3 inline-flex items-center justify-center rounded-full w-8 h-8 block bg-black text-white">
+        {order}
+      </span>
+      <span>{children}</span>
+    </div>
+  )
+}
 
 function WidgetList() {
   return (
-    <section className="flex flex-row flex-wrap">
-      <div className="basis-1/2">
-        <h2>Install widget on any website</h2>
-      </div>
-      <div className="basis-1/2">
-        <ol>
-          <li>Choose a widget from the list.</li>
-          <li>Select a place to put it on your website.</li>
-          <li>
-            Copy the piece of code to the <code>&lt;head&gt;</code> tag of your website.
-          </li>
-        </ol>
-      </div>
-      {widgetTypes.map((type) => (
-        <div key={type} className="basis-1/2 p-2">
-          <Widget type={type} />
+    <section className="widget-list mt-20 mb-20">
+      <div className="flex flex-col md:flex-row flex-wrap mb-8">
+        <div className="basis-1/2">
+          <h2 className="text-center md:text-left text-3xl md:text-5xl xl:text-6xl md:mr-10 xl:mr-20 mb-5 md:mb-0">
+            Install widget <br className="hidden md:block" /> on any website
+          </h2>
         </div>
-      ))}
+        <div className="basis-1/2">
+          <ListItem order={1}>Choose a widget from the list.</ListItem>
+          <ListItem order={2}>Select a place to put it on your website.</ListItem>
+          <ListItem order={3}>
+            Copy the piece of code to the <code>&lt;head&gt;</code> tag of your website.
+          </ListItem>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row flex-wrap">
+        {widgetTypes.map((type) => (
+          <div key={type} className="basis-1/2 p-2">
+            <Widget type={type} />
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
