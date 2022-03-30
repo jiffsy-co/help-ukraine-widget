03743aedb467bc866874b6ac214e73284b0696b2
Maxim Khmara <maxim.khmara@gearheart.io>
2022-03-30 19:57:42 +0300

    feat: prettier write /widget-script

--- widget-script/src/constants.ts
+++ widget-script/src/constants.ts
@@ -1,30 +1,37 @@
-export const donateUrl = "https://helpukrainewin.org";
-export const supportUrl = "https://war.ukraine.ua";
-export const landingUrl = "https://helpukrainewinwidget.org";
+export const donateUrl = 'https://helpukrainewin.org'
+export const supportUrl = 'https://war.ukraine.ua'
+export const landingUrl = 'https://helpukrainewinwidget.org'
 
-export const sliderStorageKey = 'huww-slider-state';
-export const sliderDisabledPersistenceStorageKey = 'huww-slider-disabled-persistence';
+export const sliderStorageKey = 'huww-slider-state'
+export const sliderDisabledPersistenceStorageKey = 'huww-slider-disabled-persistence'
 
-export const widgetWidth: number = 400;
-export const widgetHeight: number = 250;
+export const widgetWidth: number = 400
+export const widgetHeight: number = 250
 
-export const widgetPositions = ['top-left', 'top-right', 'bottom-right', 'bottom-left', 'middle-left', 'middle-right'] as const;
-export type WidgetPosition = typeof widgetPositions[number];
+export const widgetPositions = [
+  'top-left',
+  'top-right',
+  'bottom-right',
+  'bottom-left',
+  'middle-left',
+  'middle-right',
+] as const
+export type WidgetPosition = typeof widgetPositions[number]
 
-export const slideLayout = ['collapsed', 'main', 'expanded'] as const;
-export type SlideLayout = typeof slideLayout[number];
+export const slideLayout = ['collapsed', 'main', 'expanded'] as const
+export type SlideLayout = typeof slideLayout[number]
 
-export const animationDirection = ['vertical', 'horizontal'] as const;
-export type AnimationDirection = typeof animationDirection[number];
+export const animationDirection = ['vertical', 'horizontal'] as const
+export type AnimationDirection = typeof animationDirection[number]
 
-export const widgetTypes = ["one", "two", "three", "four"] as const;
-export type WidgetType = typeof widgetTypes[number];
+export const widgetTypes = ['one', 'two', 'three', 'four'] as const
+export type WidgetType = typeof widgetTypes[number]
 
-export const defaultType: WidgetType = "three";
-export const defaultPosition: WidgetPosition = "bottom-right";
-export const defaultLayout: SlideLayout = 'main';
+export const defaultType: WidgetType = 'three'
+export const defaultPosition: WidgetPosition = 'bottom-right'
+export const defaultLayout: SlideLayout = 'main'
 
-export const scriptId = 'help-ukraine-win';
-export const scriptTypeAttribute = "data-type";
-export const scriptPositionAttribute = "data-position";
-export const scriptLayoutAttribute = "data-layout";
+export const scriptId = 'help-ukraine-win'
+export const scriptTypeAttribute = 'data-type'
+export const scriptPositionAttribute = 'data-position'
+export const scriptLayoutAttribute = 'data-layout'
