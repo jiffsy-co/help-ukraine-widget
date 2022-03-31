852ac05b9ea29630b8bfe9c6f35befe4abad03ac
Maxim Khmara <maxim.khmara@gearheart.io>
2022-03-31 15:22:10 +0300

    fix: apply official page, prettier components

--- pages/install.tsx
+++ pages/install.tsx
@@ -3,11 +3,13 @@ import MainLayout from '../components/MainLayout'
 import Hero from '../components/Hero'
 import WidgetList from '../components/WidgetList'
 import WidgetHelp from '../components/WidgetHelp'
+import OfficialResources from '../components/OfficialResources'
 
 const Install: NextPage = () => {
   return (
     <MainLayout>
       <Hero />
+      <OfficialResources />
       <WidgetList />
       <WidgetHelp />
     </MainLayout>
