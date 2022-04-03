8244f09529e923df7721b4f242ef373406c1c353
Mary <maria.osadcha@sitorium.com>
2022-04-03 16:09:32 +0300

    feature privacy policy

---
+++ components/privacy/personalDataProcess/constants.tsx
@@ -0,0 +1,28 @@
+export const dataProcess = [
+  {
+    title: 'Widget data',
+    whenData: 'You download and install the widget',
+    descOfData: [
+      'read_script_tags;',
+      'write_script_tags;',
+      'e-mail;',
+      'Shopify account name (for Shopify App).',
+    ],
+    legalBasic: 'Performance of the contract',
+    reasons: 'Ensuring the operation of the product',
+  },
+  {
+    title: 'Data provided by the users',
+    whenData: 'You use our App',
+    descOfData: ['Your widget preference'],
+    legalBasic: 'Consent',
+    reasons: 'Analytics',
+  },
+  {
+    title: 'Data obtained from third parties',
+    whenData: 'You leave a review on the the Shopify App Store',
+    descOfData: ['photo;', 'name;', 'email;', 'comment;', 'evaluation.'],
+    legalBasic: 'Legitimate interest',
+    reasons: 'Analytics; support.',
+  },
+]
---
+++ components/privacy/sidebar/constans.tsx
@@ -0,0 +1,30 @@
+export const navBarLinks = [
+    {
+        name: 'Purpose of this Privacy Notice',
+        link: './'
+    },
+    {
+        name: 'Who we are',
+        link: './'
+    },
+    {
+        name: 'Personal data we process',
+        link: './'
+    },
+    {
+        name: 'Period of data storage',
+        link:  './'
+    },
+    {
+        name: 'Information of data transfer',
+        link: './'
+    },
+    {
+        name: 'Your privacy rights',
+        link:'./'
+    },
+    {
+        name: 'Update',
+        link: './'
+    }
+]
