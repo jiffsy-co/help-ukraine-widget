9bace5668c08f4324fc98cacebedb364e4e96073
Mary <maria.osadcha@sitorium.com>
2022-04-05 11:24:39 +0300

    resolve merge conflict

--- pages/who-are-we.tsx
+++ pages/who-are-we.tsx
@@ -4,25 +4,28 @@ import WhyBlock from '../components/whyBlock/WhyBlock'
 import WhoAreWeBlock from '../components/whoAreWeBlock/WhoAreWeBlock'
 import TrustedWebsites from '../components/trustedWebsites/TrustedWebsites'
 import VideoBlock from '../components/VideoBlock'
+import MainLayout from '../components/MainLayout'
 
 const WhoAreWePage: NextPage = () => {
   return (
-    <div className="bg-black flex flex-col items-center">
-      <div className="flex flex-col md:flex-row w-full">
-        <div className="md:w-2/4 md:flex md:justify-end">
-          <VideoBlock />
+    <MainLayout>
+      <div className="bg-black flex flex-col items-center">
+        <div className="flex flex-col md:flex-row w-full">
+          <div className="md:w-2/4 md:flex md:justify-end">
+            <VideoBlock />
+          </div>
+          <div className="flex flex-col w-full items-center md:w-2/4">
+            <TextBlock title="Who are we?">
+              <WhoAreWeBlock />
+            </TextBlock>
+            <TextBlock title="Why are we doing this?">
+              <WhyBlock />
+            </TextBlock>
+          </div>
         </div>
-        <div className="flex flex-col w-full items-center md:w-2/4">
-          <TextBlock title="Who are we?">
-            <WhoAreWeBlock />
-          </TextBlock>
-          <TextBlock title="Why are we doing this?">
-            <WhyBlock />
-          </TextBlock>
-        </div>
+        <TrustedWebsites />
       </div>
-      <TrustedWebsites />
-    </div>
+    </MainLayout>
   )
 }
 
