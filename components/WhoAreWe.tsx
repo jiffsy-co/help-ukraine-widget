6dad9fec27911a57637edb1921d0a69d1e5000f5
Maxim Khmara <maxim.khmara@gearheart.io>
2022-04-06 16:39:37 +0300

    Header scroll color switcher (#32)
    * fix build
    
    * feature landing
    
    * refactor buttons
    
    * refactor tailwind.config
    
    * feat: header scroll color switcher
    
    * refactor
    
    * resolve merge conflicts
    
    * fix: header scroll color switcher
    
    * feat: header scroll color switcher
    
    * fix: header scroll color switcher
    
    Co-authored-by: Mary <maria.osadcha@sitorium.com>
    Co-authored-by: MashaOsadcha98 <99804504+MashaOsadcha98@users.noreply.github.com>

--- components/WhoAreWe.tsx
+++ components/WhoAreWe.tsx
@@ -3,25 +3,28 @@ import WhyBlock from '../components/whyBlock/WhyBlock'
 import WhoAreWeBlock from '../components/whoAreWeBlock/WhoAreWeBlock'
 import TrustedWebsites from '../components/trustedWebsites/TrustedWebsites'
 import VideoBlock from '../components/VideoBlock'
+import { HeaderScrollSwitcher } from './header/HeaderScrollSwitcher'
 
 const WhoAreWe = () => {
   return (
-    <div className="bg-black flex flex-col items-center">
-      <div className="flex flex-col md:flex-row w-full mt-16">
-        <div className="md:w-2/4 md:flex md:justify-end">
-          <VideoBlock />
+    <HeaderScrollSwitcher tailwindColor="black">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col md:flex-row w-full mt-16">
+          <div className="md:w-2/4 md:flex md:justify-end">
+            <VideoBlock />
+          </div>
+          <div className="flex flex-col w-full items-center md:w-2/4">
+            <TextBlock title="Who are we?">
+              <WhoAreWeBlock />
+            </TextBlock>
+            <TextBlock title="Why are we doing this?">
+              <WhyBlock />
+            </TextBlock>
+          </div>
         </div>
-        <div className="flex flex-col w-full items-center md:w-2/4">
-          <TextBlock title="Who are we?">
-            <WhoAreWeBlock />
-          </TextBlock>
-          <TextBlock title="Why are we doing this?">
-            <WhyBlock />
-          </TextBlock>
-        </div>
+        <TrustedWebsites />
       </div>
-      <TrustedWebsites />
-    </div>
+    </HeaderScrollSwitcher>
   )
 }
 
