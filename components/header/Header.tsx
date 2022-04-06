474b9446f3da85c38932bc4f9f6badbec6e3258b
Maxim Khmara <maxim.khmara@gearheart.io>
2022-04-06 14:48:02 +0300

    feat: header scroll color switcher

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,65 +1,60 @@
-import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-// @ts-ignore
-import Scrollspy from 'react-scrollspy'
 import ShopifyButton from './ShopiffyButton'
 import Button from '../customComponents/Button'
 import Logo from '../../public/logo.png'
 import { installManuallyLink, privacyPolicyLink, home } from '../constants'
+import { useColorSwitcher } from './HeaderScrollSwitcher'
 
-const Header = (props: any) => {
+const Header = () => {
+  useColorSwitcher()
   return (
-    <>
-      <Scrollspy items={['widget']} currentClassName="is-current-header" offset={450}>
-        <div className="hidden md:flex w-screen justify-evenly bg-inherit items-center justify-around h-20 lg:h-22 xl:h-24  border-b border-solid border-borderGray">
-          <div>
-            <Link href={installManuallyLink}>
-              <span className="mr-2.5 text-black font-bold text-sm leading-4 opacity-70 hover:opacity-100">
-                Install manually
-              </span>
-            </Link>
-            <Link href={privacyPolicyLink}>
-              <span className="text-black font-bold text-sm leading-4 opacity-70 hover:opacity-100">
-                Privacy Policy
-              </span>
-            </Link>
+    <header className="sticky top-0 z-10 transition bg-white">
+      <div className="hidden md:flex w-screen justify-evenly bg-inherit items-center justify-around h-20 lg:h-22 xl:h-24  border-b border-solid border-borderGray">
+        <div>
+          <Link href={installManuallyLink}>
+            <span className="mr-2.5 text-black font-bold text-sm leading-4 opacity-70 hover:opacity-100">
+              Install manually
+            </span>
+          </Link>
+          <Link href={privacyPolicyLink}>
+            <span className="text-black font-bold text-sm leading-4 opacity-70 hover:opacity-100">
+              Privacy Policy
+            </span>
+          </Link>
+        </div>
+        <Link href={home}>
+          <div className="flex items-center">
+            <div className="h-12 w-12 rounded-3xl">
+              <Image src={Logo} />
+            </div>
+            <span className="leading-6 text-blue-ua font-bold text-2xl ml-2.5">
+              Help Ukraine Widget
+            </span>
           </div>
-          <Link href={home}>
-            <div className="flex items-center">
-              <div className="h-12 w-12 rounded-3xl">
+        </Link>
+        <ShopifyButton />
+      </div>
+      <div className="md:hidden w-full overflow-y-scroll ">
+        <div className="flex w-fit">
+          <div className="w-32">
+            <div className="flex items-center w-32 h-10">
+              <div className="h-10 w-10 mt-1.5">
                 <Image src={Logo} />
               </div>
-              <span className="leading-6 text-blue-ua font-bold text-2xl ml-2.5">
-                Help Ukraine Widget
-              </span>
-            </div>
-          </Link>
-          <ShopifyButton />
-        </div>
-        <div className="md:hidden w-full overflow-y-scroll ">
-          <div className="flex w-fit">
-            <div className="w-32">
-              <div className="flex items-center w-32 h-10">
-                <div className="h-10 w-10 mt-1.5">
-                  <Image src={Logo} />
-                </div>
-                <span className="leading-6 text-blue-ua font-bold text-sm">
-                  Help Ukraine Widget
-                </span>
-              </div>
-            </div>
-            <ShopifyButton />
-            <div className="w-32 mr-2.5 md:mr-0 h-12">
-              <Button title="Install manually" link={installManuallyLink} />
-            </div>
-            <div className="w-32  mr-2.5 md:mr-0 h-12">
-              <Button title="Privacy Policy" link={privacyPolicyLink} />
+              <span className="leading-6 text-blue-ua font-bold text-sm">Help Ukraine Widget</span>
             </div>
           </div>
+          <ShopifyButton />
+          <div className="w-32 mr-2.5 md:mr-0 h-12">
+            <Button title="Install manually" link={installManuallyLink} />
+          </div>
+          <div className="w-32  mr-2.5 md:mr-0 h-12">
+            <Button title="Privacy Policy" link={privacyPolicyLink} />
+          </div>
         </div>
-      </Scrollspy>
-    </>
+      </div>
+    </header>
   )
 }
 
